7e287ef4589293617e9944bf9ce7ac130a7e0683
Joe Rezendes <rezenbiz@gmail.com>
2018-03-23 11:46:48 -0700

    Main sequence done

--- public/scripts/rootscripts.js
+++ public/scripts/rootscripts.js
@@ -3,5 +3,19 @@ $('.what').on("click", function() {
   $('.hero').fadeOut(3000);
     $('.hero2').css('transform', 'scale(0)')
     $('.titleEx').delay(5000).fadeIn(5000);
-$('.title2').delay(15000).fadeIn(3000);
+    // $('.title2').delay(15000).fadeIn(3000);
+    $('.divinevid').delay(20000).fadeIn(3000, function() {
+      // Get the video
+      var video = document.getElementById("myVideo");
+      var btn = document.getElementById("myBtn");
+
+
+
+      video.play();
+
+      $('.divinevid').delay(60 * 1000).fadeOut(3000);
+      $('.hero').delay(60 * 1000).fadeIn(3000);
+
+    })
+
 })
