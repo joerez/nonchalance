80aeb8953f7176e2b3f8992c306fa3fe713918a9
Joe Rezendes <rezenbiz@gmail.com>
2018-03-22 17:11:24 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const methodOverride = require('method-override');
+
+//initialize express
+const app = express();
+
+const bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/nonchalance', {  });
+
+//Middleware
+
+//bodyparser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//methodOverride
+app.use(methidOverride('_method'));
+
+//Set up handlebars
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+
+//end middleware
+
+//ROUTES
+require('./controllers/root.js');
+
+
+const port = process.env.PORT || 6000
+
+app.listen(port, () => {
+  console.log(`Nonchalance listening on port ${port}!`);
+})
